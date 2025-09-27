572d68311fcb2255e7af92f15f1f36cf897c2180
ed neville <ed@s5h.net>
2025-09-27 11:30:25 +0100

    correcting split

--- cdb_maker.pl
+++ cdb_maker.pl
@@ -43,7 +43,7 @@ sub process_add {
     while (my $line = <$fh>) {
         chomp $line;
         if ($line =~ /^\s*(\d+)\s+(.*)$/) {
-            my ($desc,$cc) = split /,\s*/, $2, 2;
+            my ($desc,$cc) = split(/,\s*([^,]+)$/, $2);
             if (defined $desc && defined $cc) {
                 $net->{$1} = "ASNDESC=$desc,ASNCC=$cc,ASN=$1,NET=$2";
             }
